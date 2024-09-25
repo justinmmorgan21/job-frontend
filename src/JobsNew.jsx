95745208400c805f0d0616c55b8e9d23fa410468
justinmmorgan21 <justinmorgan21@gmail.com>
2024-09-24 19:54:02 -0500

    add create action

---
+++ src/JobsNew.jsx
@@ -0,0 +1,27 @@
+export function JobsNew(props) {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    console.log(params);
+    props.onCreate(params, () => event.target.reset());
+  }
+
+  return (
+    <>
+    <form id="new-job" onSubmit={handleSubmit}>
+      <label htmlFor="title">Job:</label>
+      <input type="text" id="title-input" name="title" /><br />
+      <label htmlFor="date-time">Start date:</label>
+      <input type="datetime-local" id="date-time-input" name="date-time" /><br />
+      <label htmlFor="duration">Duration:</label>
+      <input type="text" id="duration-input" name="duration"/><br />
+      <label htmlFor="details">Details:</label>
+      <textarea id="details-input" name="details" rows="5"></textarea><br />
+      <label htmlFor="user-id">user:</label>
+      <input type="text" id="user-input" name="user-id" /><br />
+      <button type="submit">Submit</button>
+    </form>
+    </>
+  );
+}
--- src/JobsPage.jsx
+++ src/JobsPage.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import {JobsIndex} from "./JobsIndex";
+import {JobsNew} from "./JobsNew";
 export function JobsPage() {
 
   const[jobs, setJobs] = useState([]);
@@ -11,10 +12,19 @@ export function JobsPage() {
     })
   }
 
+  const handleCreate = (params, successCallback) => {
+    console.log("Create");
+    axios.post("http://localhost:3000/jobs.json", params).then(response => {
+      setJobs([...jobs, response.data]);
+      successCallback();
+    })
+  }
+
   useEffect(handleIndex, []);
 
   return (
     <main>
+      <JobsNew onCreate={handleCreate}/>
       <JobsIndex jobs={jobs} onIndex={handleIndex}/>
     </main>
   )
